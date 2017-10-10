65bc520fdefe61521bb48fe773eb64b349027532
Christos Manios <maniopaido@gmail.com>
2017-10-10 11:57:14 +0300

    Copy initial conf files if conf dir is empty (may be mounted as volume to host) and start Varnish properly.

--- 5.2/start.sh
+++ 5.2/start.sh
@@ -3,6 +3,7 @@
 set -e
 
 CONF_DIR='/etc/varnish'
+PIDFILE=/run/varnish.pid
 
 # Check if conf directory is empty as it might be mounted as a volume to the host
 # https://www.cyberciti.biz/faq/linux-unix-shell-check-if-directory-empty/
@@ -13,8 +14,16 @@ else
   cp -Rp /orig/conf/* "$CONF_DIR"/
 fi
 
+# Print Varnish version before starting
+/usr/sbin/varnishd -V
+
+# Start Varnish
 exec bash -c \
-  "exec varnishd -F -u varnish \
+  "exec varnishd -F \
+  -a :6081 \
+  -T localhost:6082 \
+  -S /etc/varnish/secret \
+  -P $PIDFILE \
   -f $VCL_CONFIG \
   -s malloc,$CACHE_SIZE \
   $VARNISHD_PARAMS"
